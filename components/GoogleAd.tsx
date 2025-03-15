f34e1d49b78ecdeac95829822d65a6afb3508b60
Md Istiak Ahmed <istinishat@gmail.com>
2025-03-15 17:08:39 +0600

    fix bug

--- components/GoogleAd.tsx
+++ components/GoogleAd.tsx
@@ -1,29 +1,36 @@
 "use client";
-
-import React from 'react';
+import React, { useEffect } from "react";
 
 const GoogleAd = ({ adSlot }: { adSlot: string }) => {
-    return (
-        <div>
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9516918341435316"
-            crossOrigin="anonymous"
-          ></script>
-          {/* <!-- Rectangular ad unit --> */}
-          <ins
-            className="adsbygoogle"
-            style={{ display: "block" }}
-            data-ad-client="ca-pub-9516918341435316"
-            data-ad-slot={adSlot}
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-          ></ins>
-          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-        </div>
-      );
-  };
 
 
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.adsbygoogle) {
+      if (!window.adsbygoogle.loaded) {
+        window.adsbygoogle.loaded = true;
+        window.adsbygoogle.push({});
+      }
+    }
+  }, []);
+  
+
+  return (
+    <div>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9516918341435316"
+        crossOrigin="anonymous"
+      ></script>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-9516918341435316"
+        data-ad-slot={adSlot}
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+    </div>
+  );
+};
 
 export default GoogleAd;
--- next.config.ts
+++ next.config.ts
@@ -10,10 +10,12 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://pagead2.googlesyndication.com;
-              img-src 'self' https://pagead2.googlesyndication.com https://www.google.com;
-              frame-src 'self' https://googleads.g.doubleclick.net https://www.google.com;
-              frame-ancestors 'self' https://google.com https://googleads.g.doubleclick.net;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://pagead2.googlesyndication.com https://www.googletagmanager.com https://www.google-analytics.com https://*.google.com https://*.adtrafficquality.google;
+              img-src 'self' https://pagead2.googlesyndication.com https://www.google.com https://*.google.com https://*.adtrafficquality.google;
+              frame-src 'self' https://googleads.g.doubleclick.net https://www.google.com https://*.google.com https://*.adtrafficquality.google;
+              style-src 'self' 'unsafe-inline';
+              connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://*.google.com https://*.adtrafficquality.google;
+              frame-ancestors 'self' https://google.com https://googleads.g.doubleclick.net https://*.google.com https://*.adtrafficquality.google;
             `.replace(/\s{2,}/g, " "), // Minify CSP string
           },
         ],
