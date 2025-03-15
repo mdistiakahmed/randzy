72572dd12d5d1050a3dffabf180212464b4054ab
Md Istiak Ahmed <istinishat@gmail.com>
2025-03-15 16:54:56 +0600

    fix error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://pagead2.googlesyndication.com;
+              img-src 'self' https://pagead2.googlesyndication.com https://www.google.com;
+              frame-src 'self' https://googleads.g.doubleclick.net https://www.google.com;
+              frame-ancestors 'self' https://google.com https://googleads.g.doubleclick.net;
+            `.replace(/\s{2,}/g, " "), // Minify CSP string
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
